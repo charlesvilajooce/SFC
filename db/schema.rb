836a9ecd0ead60ffa2c50328a29e84ab4d68288b
charlesvila <charles.vila@jooce.ch>
2017-07-04 00:28:48 +0200

    GameCenter functionnal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703123829) do
+ActiveRecord::Schema.define(version: 20170703162501) do
 
   create_table "archived_infos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -255,6 +255,7 @@ ActiveRecord::Schema.define(version: 20170703123829) do
     t.string   "mothercontact"
     t.string   "fathercontact"
     t.integer  "numberofsiblings"
+    t.string   "alias"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
