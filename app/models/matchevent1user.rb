91c5cd57dd6928ecbb0e466be37b2684a7b74b1e
cadibiss <bernard.moret@jooce.ch>
2017-07-16 07:35:03 +0200

    Finalisation 2 Bernard

--- app/models/matchevent1user.rb
+++ app/models/matchevent1user.rb
@@ -13,7 +13,7 @@ class Matchevent1user < ApplicationRecord
         when "Carton Rouge"
           return (self.user.getname()+" a recu un carton rouge")
         when "Passe"
-          return ("Passe de "+self.user.getname()+".")
+          return ("Passe de "+self.user.getname())
         else
           return ""
       end
