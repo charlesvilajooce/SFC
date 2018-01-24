784ad11d2ef14e2a6ab87fb434cf4c958903d0ef
cadibiss <bernard.moret@jooce.ch>
2018-01-24 18:24:05 +0100

    Migration preparation for parents infos

---
+++ db/migrate/20171114163513_add_parentsinfos_to_users.rb
@@ -0,0 +1,12 @@
+class AddingParentsinfosToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :nomprenompere, :string
+    add_column :users, :nomprenommere, :string
+    add_column :users, :mailpere, :string
+    add_column :users, :mailmere, :string
+    add_column :users, :prixcotisations, :string
+    add_column :users, :remarquesgenerales, :string
+    add_column :users, :lieunaissance, :string
+    add_column :users, :telportablejoueur, :string
+  end
+end
